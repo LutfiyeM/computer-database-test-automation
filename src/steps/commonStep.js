709c0481ed9f33902fe83191b78be9830df53c92
BTPRO_Air <btproair@BTPRO-Air-MacBook-Air.local>
2020-10-11 21:53:47 +0100

    Code cleaning

--- src/steps/commonStep.js
+++ src/steps/commonStep.js
@@ -2,6 +2,7 @@ import { Given } from 'cucumber';
 
 import openWebsite from '../support/action/openWebsite';
 
+// Open the website
 Given(/^I open the url$/, () => {
     openWebsite("url","http://computer-database.herokuapp.com/computers")
 });
